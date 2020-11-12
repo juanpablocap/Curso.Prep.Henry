6b50541bf6ccdf2a78ddce0e8e99e331bf1300cb
JP <juanpablocapilla@gmail.com>
2020-11-12 14:24:52 -0300

    Homework 2 done

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -78,7 +78,7 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  If (numero % 3 == 0 && numero % 5 == 0){
+  if (numero % 3 == 0 && numero % 5 == 0){
     return "fizzbuzz";
   }else if (numero % 3 == 0){
     return "fizz";
